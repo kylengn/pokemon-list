5426507cd75dcd5f7bc435838499e8868ba30d3a
Kyle Nguyen <kyle.nguyen304@gmail.com>
2024-09-21 20:07:43 +0700

    fix: card image size & quality

--- components/pokemon-card.tsx
+++ components/pokemon-card.tsx
@@ -12,7 +12,7 @@ export type PokemonCardProps = {
 
 export const PokemonCard = ({ pokemon }: PokemonCardProps) => {
   return (
-    <Card className="w-60 h-80 relative overflow-hidden rounded-none border-none shadow-none transition-all hover:scale-105">
+    <Card className="w-60 h-[21rem] relative overflow-hidden border-none shadow-none transition-all hover:scale-105 rounded-xl hover:drop-shadow-xl">
       {/* TODO: Just a sample placeholder for now, will replace with actual pokemon placeholder image based on the pokemon's type later */}
       <Image
         src="https://www.nicepng.com/png/detail/241-2416628_tcg-blanks-neo-jumbo-pokemon-trading-card-blank.png"
@@ -20,23 +20,25 @@ export const PokemonCard = ({ pokemon }: PokemonCardProps) => {
         fill
         sizes="100%"
         priority
-        className="z-0 object-cover"
+        className="object-cover scale-[1.08]"
       />
       <CardContent className="p-4 relative z-10 h-full flex flex-col justify-between">
         <div className="text-left">
           <span className="font-bold px-2">#{pokemon.id}</span>
         </div>
-        <div className="flex justify-center items-start h-48 mb-10">
+        <div className="flex justify-center items-start h-48">
           <Image
             src={pokemon.image}
             alt={pokemon.name}
-            width={136}
-            height={136}
-            className="drop-shadow-lg"
+            width={100}
+            height={100}
+            sizes="100%"
+            priority
+            className="object-cover"
           />
         </div>
         <div className="text-center">
-          <h2 className="text-sm font-bold tracking-wide capitalize mb-1.5">{pokemon.name}</h2>
+          <h2 className="text-sm font-bold tracking-wide capitalize mb-1">{pokemon.name}</h2>
           {/* <span className={`px-3 py-1 rounded-full text-sm font-semibold text-white bg-${pokemon.type.toLowerCase()}`}>
             {pokemon.type}
           </span> */}
